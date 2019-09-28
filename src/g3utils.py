841db5b6f1f6c97e10f6141bba9e2f3ded283c87
Charly Pereiro <capereiro@gmail.com>
2019-09-28 00:26:28 -0300

    update

--- src/g3utils.py
+++ src/g3utils.py
@@ -5,6 +5,71 @@ import re as re
 
 # ------------------------------
 
+def obtener_df_indexado_simple(df, ncol, clave=None):
+    """
+    Busca en datos no-None y confecciona un data frame indexado incluyendo nombres a las columnas.
+    Adicionalmente puede buscar por una clave distinta de None que puede ingresarse como parámetro.
+    A diferencia de 'obtener_df_indexado', 'obtener_df_indexado_simple' operara sobre estructuras de datos simples como un str o int
+
+    Parameters:
+    -----------
+    arg : df, ncol, clave [Opcional, default = None]
+    
+    df -- data frame
+    ncol -- nombre de columna indexada
+    clave -- parámetro opcional. Clave distinta de None. Por defecto es None
+    
+    Returns:
+    --------
+    df_ret : data frame de dos dimensiones (indice & ncol).
+    
+    """
+
+    l_ind = []
+    l_col = []
+    for i in range(len(df.index)):
+        if df.iloc[i] != clave:
+            l_ind.append(i)
+            l_col.append(df.iloc[i])
+    df_ret = pd.DataFrame(l_ind, columns={'indice'})
+    df_ret[ncol] = pd.DataFrame(l_col)
+    return df_ret
+
+# ------------------------------
+
+def registrar_recupero(valor, colu, df_recup):
+    """
+    Registra cantidad de valores recuperados de un feature en particular.
+    Devuelve un data frame con los valores recuperados.
+    Adicionalmente deja un archivo.csv a modo de syslog
+    
+    Parameters:
+    -----------
+    arg : valor, colu
+    
+    valor -- cant recuperada de datos del feature
+    colu -- nombre de columna (feature)
+    
+    Returns:
+    --------
+    ret : data frame de una dimensión y archivo.csv
+    
+    """
+    if df_recup.size == 0:     
+        #recup = pd.DataFrame([valor], columns=[colu])
+        df_recup = pd.DataFrame([valor], columns=[colu])
+        print('primer dato en df')
+    elif colu not in df_recup.columns:
+        df_recup[colu] = [valor+1]
+        print('columna creada')
+    else:
+        df_recup[colu] = [valor+2]
+        print('columna existente')
+    df_recup.to_csv(r'../data/valores_recuperados.csv', encoding='utf-8')
+    return df_recup
+
+# ------------------------------
+
 def obtener_df_indexado(df, ncol, clave=None):
     """
     Busca en datos no-None y confecciona un data frame indexado incluyendo nombres a las columnas.
@@ -55,7 +120,7 @@ def limpiar_columna_x_clave(dic, df):
     
     """
     serie_1 = pd.Series([x if x not in dic else dic.get(x) for x in df])
-    serie_2 = pd.to_numeric(serie_1, errors='coerse', downcast='float') # coerse: pasa a NaN los no-numericos
+    serie_2 = pd.to_numeric(serie_1, errors='coerse', downcast='float') # coerse: pasa a los no-numericos como Nan's
     return pd.DataFrame(serie_2)
 
 # ------------------------------
@@ -105,3 +170,31 @@ def generar_df_posproduccion(nfilas):
     """
     pos_pro = np.arange(0,nfilas)
     return pd.DataFrame(pos_pro, columns={'indice'})
+
+
+# ------------------------------
+
+def existe_clave(key, columna, df_aux):    
+    """
+    Verifica existencia de una key dentro de la columna de un data frame
+    
+    Parameters:
+    -----------
+    arg : key, columna, df_aux
+    
+    key -- clave de búsqueda
+    columna -- nombre de columna a realizar la búsqueda
+    df_aux -- data frame de búsqueda
+    
+    Returns:
+    --------
+    ret : cantidad de valores encontrados
+    """
+    
+    m = []
+    for frase in df_aux[columna]:
+        if key in frase:
+            m.append(True)
+        else:
+            m.append(False)
+    return sum(m)
